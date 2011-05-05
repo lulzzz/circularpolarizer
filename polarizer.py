08c989fa7b0ba569484ede07826d56d43f9cc9bb
Michael Schneider <260685s@gmail.com>
2011-05-05 16:19:00 +0200

    new start...using tabulated optical constants and transfer-matrices

--- polarizer.py
+++ polarizer.py
@@ -2,89 +2,123 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.constants import e, hbar
-from os import getcwd
+from scipy.constants import e,c,h
+
+# calculate the complex refraction coefficient (Fresnel) for a multilayer-structure
+# matrices are from:
+# Harland G. Tompkins, Eugene A. Irene: 'Handbook of ellipsometry' 
+
+# Structure looks like this:
+# 
+# | Layer 0: Vacuum (n_vac, phi_vac) | Layer 1: e.g. B4C (n1, phi1, b1, d1) | Layer N: (...) | Layer N+1: Substrate (n_sub, phi_sub)
+#
+# input data should look like:
+# ambient = [ phi, n , lambda ]
+# layerlist = [ [ d_1, n_1, l_1 ], ... , [ d_N, n_N, l_N ] ]
+# substrate = [ n ]
 
 # {{{ definitions
 
-# Müller-Matrix
-M = lambda phi,delta,rp,rs: (rp**2+rs**2)/2*np.matrix([
-    [1,-np.cos(2*phi),0,0], \
-    [-np.cos(2*phi),1,0,0], \
-    [0,0,np.sin(2*phi)*np.cos(delta),np.sin(2*phi)*np.sin(delta)], \
-    [0,0,-np.sin(2*phi)*np.sin(delta),np.sin(2*phi)*np.cos(delta)]])
+lambda2eV = lambda l: h * c / ( e * l )
+eV2lambda = lambda E: h * c / ( e * E )
 
-# Stokes-Vektor
-S = lambda alpha: np.array([1, np.cos(2*alpha), np.sin(2*alpha), 0])
+ii = np.complex(0, 1)
 
-# Kramers-Kronig-Relation: 
-# calculate the imaginary part of reflectivity (=phaseshift) at given circ.frequency
-# omega: given frequency
-# _omega: list of all frequencies to be 'integrated' (=sum) over
-# _refl: corresponding list of reflectivities
-# kk_phase = lambda omega, _omega, _refl: \
-        # omega/np.pi * sum([ np.log(_refl[i]/_refl[np.where(_omega == omega)])/(om**2 - omega**2) \
-        # for i, om in enumerate(_omega) if om!=omega ])
+b_factor = lambda d, n, phi, l: 2 * np.pi * d * n * np.cos(phi) / l
 
-# convert Energy(eV) -> ω
-eV2omega = lambda eV: eV*e/hbar
+refracted = lambda phi, n1, n2: np.arcsin( np.sin(phi) * n1/ n2 )
 
-# import dat-files from CXRO-bilayer-refl-calculator, returns angles, reflectivities
-def importindex(filename):        
-    return np.genfromtxt(filename, skip_header=2, unpack=True)
+# matrix for ambient layer, p- and s-polarization
+# angle of incidence phi is measured from surface-normal!
+def M_amb(phi, n, polarisation):
+    if polarisation == 'p':
+        return .5 * np.matrix( [ [ 1, np.cos(phi)/n ], [ -1, np.cos(phi)/n ] ] )
+    elif polarisation == 's':
+        return .5 * np.matrix( [ [ 1, 1 / ( np.cos(phi) * n ) ], [ 1, -1 / ( np.cos(phi) * n ) ] ] )
+    
+# same for arbitrary layer of thickness d, index n, angle of incidence phi (REFRACTED!)
+# at wavelength l
+def M_lay(d, n, phi_n, l, polarisation):
+    b = b_factor( d, n, phi_n, l )
+    if polarisation == 'p':
+        M = np.matrix( \
+                [ [ np.cos(b), -ii * np.cos(phi_n) * np.sin(b) / n ],\
+                [ ii * n * np.sin(b) / np.cos(phi_n), np.cos(b) ] ] )
+        return M
+    elif polarisation == 's':
+        M = np.matrix( \
+                [ [ np.cos(b), ii * np.sin(b) / ( n * np.cos(phi_n) ) ],\
+                [ ii * n * np.sin(b) * np.cos(phi_n), np.cos(b) ] ] )
+        return M
 
-# put it all together...
-# def phaseshift(filename, energy):
-    # _energy, reflectivity = importdat(filename)
-    # phaseshift = kk_phase(eV2omega(energy), eV2omega(_energy), reflectivity)
-    # return phaseshift
+# matrices for an infinite half-space ( =substrate )
+def M_sub(phi_n, n, polarisation):
+    if polarisation == 'p':
+        return np.matrix( [ [ np.cos(phi_n) / n, 0 ], [ 1, 0 ] ] )
+    elif polarisation == 's':
+        return np.matrix( [ [ 1 / ( np.cos(phi_n) * n ), 0 ], [ 1, 0 ] ] )
 
-# refl_i = complex reflection coeff. ~ r*exp(iφ)
-refl_s = lambda eps, th: eps*np.sin(th) - np.sqrt((eps-np.cos(th)**2))\
-        /(eps*np.sin(th) + np.sqrt(eps-np.cos(th)**2))
+# M_ges = M_amb * M_lay1 * ... * M_layN * M_sub
+def M_ges(ambient, layerlist, substrate, polarisation):
+    M = M_amb( ambient[0], ambient[1], polarisation )               # starting with vacuum
+    phi_n, n_old, l_0 = ambient
+    for layer in layerlist:                             # going through the finite-thickness layers
+        d, n_new = layer
+        l = l_0 / n_new
+        phi_n = refracted(phi_n, n_old, n_new)
+        M = M * M_lay(d, n_new, phi_n, l, polarisation)
+        n_old = n_new
+    n_new = substrate[0]                                   # last step: infintie-half-space substrate
+    phi_n = refracted(phi_n, n_old, n_new)
+    M = M * M_sub( phi_n, n_new, polarisation )
+    return M
 
-refl_p = lambda eps, th: np.sin(th) - np.sqrt((eps-np.cos(th)**2))\
-        /(np.sin(th) + np.sqrt(eps-np.cos(th)**2))
+def _test_M_ges(ambient, layerlist, substrate, polarisation):
+    M = M_amb( ambient[0], ambient[1], polarisation )               # starting with vacuum
+    phi_n, n_old, l_0 = ambient
+    for layer in layerlist:                             # going through the finite-thickness layers
+        d, n_new = layer
+        l = l_0 / n_new
+        phi_n = refracted(phi_n, n_old, n_new)
+        M = M * M_lay(d, n_new, phi_n, l, polarisation)
+        n_old = n_new
+    n_new = substrate[0]                                   # last step: infintie-half-space substrate
+    phi_n = refracted(phi_n, n_old, n_new)
+    M = M * M_sub( phi_n, n_new, polarisation )
+    return M
 
-# get phase φ from complex number C with abs(C)=r: C=r*exp(іφ)
-cplx_phase = lambda cplx: np.arctan(cplx.real/cplx.imag)
+# Fresnel-reflectivity from M_ges
+r = lambda M: M[ 1, 0 ] / M[ 0, 0 ]
 
-# convert complex refractive index N=n+ik to complex dielectric constant
-n2eps = lambda n, k: np.complex(n**2 - k**2, 2*n*k)
+# phase of complex number
+cplx_phase = lambda c: np.arctan( c.imag / c.real )
 
-# closest match-finder
-closest = lambda liste, match: min(list(liste), key = lambda i: abs(i-match))
 # }}}
 
-startdir = '/home/dscran/Documents/promotion/circularpolarizer'
-# phasediff = [ phaseshift('%s/data/deg%02d_p.dat' % (startdir, angle), energy) \
-        # - phaseshift('%s/data/deg%02d_s.dat' % (startdir, angle), energy) for angle in angles ]
+# n_B4C @ 60eV
+n_B4C = np.complex( 1-0.0850091055, -0.018499583 )
+# n_Mo @ 60eV
+n_Mo = np.complex( 1-0.2158086, -0.12803553 )
 
-target_energy = 60              #eV
+n_vac = np.complex( 1, 0 )
+n_SiO2 = np.complex( 1-0.0544616096, -0.0341861285 )
 
-# the refractive index data files contain [E(eV), δ, β] with n=1-δ-iβ
-energy, delta, beta = importindex(startdir + '/data/index_B4C.dat')
+layerlist = [ [ 3e-9, n_B4C ], [ 100e-9, n_Mo ] ]
+substrate = [ n_SiO2 ]
+l = eV2lambda(60)
 
-delta = 1 - delta[ energy == closest(energy, target_energy) ]
-beta = -beta[ energy == closest(energy, target_energy) ]
+angles = np.arange( 0.01, np.pi/2, .05 )
 
-eps = n2eps( delta, beta )
+# shift = [ cplx_phase( r( M_ges( [a, n_vac, l], layerlist, substrate, 's' ) ) ) \
+        # - cplx_phase( r( M_ges( [a, n_vac, l], layerlist, substrate, 'p' ) ) ) for a in angles ]
 
-theta = np.arange(0, np.pi/2, .01)
-
-phi_s = np.array( [ cplx_phase( refl_s(eps, th) ) for th in theta ] )
-phi_p = np.array( [ cplx_phase( refl_p(eps, th) ) for th in theta ] )
-
-D_phi = phi_s - phi_p
-
-# {{{ Plotten
+r_s = [ r( M_ges( [a, n_vac, l], layerlist, substrate, 's' ) ).real for a in angles ]
+r_p = [ r( M_ges( [a, n_vac, l], layerlist, substrate, 'p' ) ).real for a in angles ] 
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-plt.plot(np.rad2deg(theta), np.rad2deg(D_phi), 'g-')
-# plt.plot(angles, reflectivities_s, 'b-')
-fig.savefig(startdir+'/phase.pdf')
-
-# }}}
-
+plt.plot(np.pi/2-angles, r_s, 'r-', label='s')
+plt.plot(np.pi/2-angles, r_p, 'g-', label='p')
+plt.legend()
+fig.savefig('/home/dscran/Documents/promotion/circularpolarizer/phase.pdf')
 # vim: foldmethod=marker
